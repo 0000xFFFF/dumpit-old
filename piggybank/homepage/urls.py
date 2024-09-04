3adb3a761b0114d3d64bdf8bd6c319e92d9e9907
0000xFFFF <coldmailgg@gmail.com>
2024-09-05 01:55:04 +0200

    small changes

--- piggybank/homepage/urls.py
+++ piggybank/homepage/urls.py
@@ -2,11 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('index', views.index),
-    path('index/', views.index),
-    path('home', views.index),
-    path('home/', views.index),
-    path('login', views.login),
-    path('login/', views.login),
+    path('', views.index_view, name="index"),
+    path('home', views.index_view),
+    path('index', views.index_view),
+    path('login', views.login_view),
 ]
--- piggybank/homepage/views.py
+++ piggybank/homepage/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .form import ContactForm
 
-def index(request):
+def index_view(request):
     return render(request, 'index.html')
 
-def login(request):
+def login_view(request):
     return render(request, "login.html")
