36e4b7c9d3d3df03b467ec43af33e6a3cdec93b0
0000xFFFF <coldmailgg@gmail.com>
2024-09-04 23:27:05 +0200

    tut1 - setup

--- piggybank/piggybank/settings.py
+++ piggybank/piggybank/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!3q+5=v-r^p13hss8@!n6m9gzwk*%0btf#3$+z4bkblpj*es(a"
+SECRET_KEY = "django-insecure-94362%@hafl#l+5y4t+%&q(9@23i8%k2jw(^_jt4^4#h2^b#&^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "homepage.apps.HomepageConfig",
 ]
 
 MIDDLEWARE = [
