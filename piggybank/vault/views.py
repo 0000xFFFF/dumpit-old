5bc04bcd891cc31ab6d7d69ea33d106967e3067b
0000xFFFF <coldmailgg@gmail.com>
2024-09-05 17:18:39 +0200

    AGHH MAKE THIS WORKgit status!

--- piggybank/vault/views.py
+++ piggybank/vault/views.py
@@ -1,18 +1,31 @@
+import os
+from django.conf import settings
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from .forms import FileUploadForm
+from .forms import SimpleFileUploadForm
 
 def vault(request):
     return render(request, "vault.html")
 
-def file_upload_view(request):
+def handle_file_upload(uploaded_file):
+    save_path = os.path.join(settings.MEDIA_ROOT, 'uploads', uploaded_file.name)
+    with open(save_path, 'wb+') as destination:
+        for chunk in uploaded_file.chunks():
+            destination.write(chunk)
+    return save_path
+
+def upload_file_view(request):
     if request.method == 'POST':
-        form = FileUploadForm(request.POST, request.FILES)
+        form = SimpleFileUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()  # This saves the file and title to the database
-            return redirect('success_url')  # Replace with your success URL
+            # Save the file to the filesystem
+            file_path = handle_file_upload(request.FILES['file'])
+            return redirect('upload_success')  # or return a file path for the user
     else:
-        form = FileUploadForm()
+        form = SimpleFileUploadForm()
     
     return render(request, 'upload.html', {'form': form})
+
+def upload_success_view(request):
+    return render(request, 'upload_success.html')
