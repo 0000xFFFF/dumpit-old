99fefb8423a318a435777e50aa048ebc790012e8
0000xFFFF <coldmailgg@gmail.com>
2025-08-16 02:40:56 +0200

    shorten

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
-sudo ln -sfr dumpit /usr/local/bin/dumpit
-sudo ln -sfr dumpit-nossl /usr/local/bin/dumpit-nossl
+sudo ln -sfr dumpit /usr/local/bin/.
+sudo ln -sfr dumpit-nossl /usr/local/bin/.
 
